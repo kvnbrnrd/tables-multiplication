6eb99b7d3a7a63a73844f7891cbad1747f7d71a2
kvnbrnrd <kevos.directos@gmail.com>
2018-10-29 15:54:43 +0100

    ajout checkbox affichage tables

--- php.php
+++ php.php
@@ -1,9 +1,8 @@
 <?php
 
-$categorie = mysql_real_escape_string($_POST['categorie']);
 for ($i = 1; $i <= 10; $i++)
 {
-    Echo 'Table de multiplication de '.$i.'<br>';
+    echo 'Table de multiplication de '.$i.'<br>';
     for ($j = 1; $j <= 10; $j++)
     {
         echo $i.' x '.$j.' = '.$i*$j.'<br>';
