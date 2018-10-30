c84f6bcaf6fa57fa3c1a28af43d471740b075814
kvnbrnrd <kevos.directos@gmail.com>
2018-10-30 13:57:16 +0100

    ajout checkbox

--- php.php
+++ php.php
@@ -1,22 +1,51 @@
-<head><link rel="stylesheet" type="text/css" href="index2.css"></head>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="index2.css">
+    <title>Les multiplications</title>
+</head>
+<body>
 <h1 class="multi">Multiplications</h1>
 
-<form method="post">
+    <form name="check" action="" method="post">
     <h2>Affichage de plusieurs tables à la fois :</h2>
+        
+        <input type="checkbox" name="table[]" value="1"> <label for="un">Table de 1</label>
+        <input type="checkbox" name="table[]" value="2"> <label for="deux">Table de 2</label>
+        <input type="checkbox" name="table[]" value="3"> <label for="trois">Table de 3</label>
+        <input type="checkbox" name="table[]" value="4"> <label for="quatre">Table de 4</label>    
+        <input type="checkbox" name="table[]" value="5"> <label for="cinq">Table de 5</label></p>
+        <input type="checkbox" name="table[]" value="6"> <label for="six">Table de 6</label>
+        <input type="checkbox" name="table[]" value="7"> <label for="sept">Table de 7</label>
+        <input type="checkbox" name="table[]" value="8"> <label for=" huit">Table de 8</label>
+        <input type="checkbox" name="table[]" value="9"> <label for="neuf">Table de 9</label>
+        <input type="checkbox" name="table[]" value="10"> <label for="dix">Table de 10</label></p>
+    
+        <input type="submit" value="Valider" name="valider">
+    </form>
+      
 
-    <input type="hidden" name="categorie" id="categorie">
-    <input type="checkbox"  value="1"> Table du 1
-    <input type="checkbox"  value="2"> Table du 2
-    <input type="checkbox"  value="3"> Table du 3
-    <input type="checkbox"  value="4"> Table du 4
-    <input type="checkbox"  value="5"> Table du 5</p>
-    <input type="checkbox"  value="6"> Table du 6
-    <input type="checkbox"  value="7"> Table du 7
-    <input type="checkbox"  value="8"> Table du 8
-    <input type="checkbox"  value="9"> Table du 9
-    <input type="checkbox"  value="10"> Table du 10</p>
-    <button type="submit" formmethod="post">Valider</button></p>
-</form>
+    <?php
+
+    $num = $_POST['check']; 
+    foreach($_POST['table'] as $valeur){
+    for ($num=1; $num <= 10; $num++) {
+    if ($num == 10){
+    
+    echo $valeur. ' x ' .$num. ' = ' .$num * $valeur.'<br></p>';
+    } else {
+    echo $valeur. ' x ' .$num. ' = ' .$num * $valeur.'<br>';
+    }
+    
+    }
+    } 
+    
+    ?>
+
+
 
 <form method="post">
     <select name="choix">
@@ -53,4 +82,5 @@ for ($j = 1; $j <= 10; $j++)
     }
 
 ?>
-
+</body>
+</html>
