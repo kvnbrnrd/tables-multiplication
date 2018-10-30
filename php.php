32cf3bceec9248112bc20b7a95d49ef444c28579
kvnbrnrd <kevos.directos@gmail.com>
2018-10-30 14:14:21 +0100

    php plus lisible

--- php.php
+++ php.php
@@ -7,80 +7,87 @@
     <link rel="stylesheet" type="text/css" href="index2.css">
     <title>Les multiplications</title>
 </head>
-<body>
-<h1 class="multi">Multiplications</h1>
 
-    <form name="check" action="" method="post">
-    <h2>Affichage de plusieurs tables à la fois :</h2>
-        
-        <input type="checkbox" name="table[]" value="1"> <label for="un">Table de 1</label>
-        <input type="checkbox" name="table[]" value="2"> <label for="deux">Table de 2</label>
-        <input type="checkbox" name="table[]" value="3"> <label for="trois">Table de 3</label>
-        <input type="checkbox" name="table[]" value="4"> <label for="quatre">Table de 4</label>    
-        <input type="checkbox" name="table[]" value="5"> <label for="cinq">Table de 5</label></p>
-        <input type="checkbox" name="table[]" value="6"> <label for="six">Table de 6</label>
-        <input type="checkbox" name="table[]" value="7"> <label for="sept">Table de 7</label>
-        <input type="checkbox" name="table[]" value="8"> <label for=" huit">Table de 8</label>
-        <input type="checkbox" name="table[]" value="9"> <label for="neuf">Table de 9</label>
-        <input type="checkbox" name="table[]" value="10"> <label for="dix">Table de 10</label></p>
-    
-        <input type="submit" value="Valider" name="valider">
-    </form>
-      
+<body>
+
+    <h1 class="multi">Multiplications</h1>
+
+<!-- Début HTML formulaire checkbox -->
+        <form name="check" action="" method="post">
+            <h2>Affichage de plusieurs tables à la fois :</h2>
+                
+                <input type="checkbox" name="table[]" value="1"> <label for="un">Table de 1</label>
+                <input type="checkbox" name="table[]" value="2"> <label for="deux">Table de 2</label>
+                <input type="checkbox" name="table[]" value="3"> <label for="trois">Table de 3</label>
+                <input type="checkbox" name="table[]" value="4"> <label for="quatre">Table de 4</label>    
+                <input type="checkbox" name="table[]" value="5"> <label for="cinq">Table de 5</label></p>
+                <input type="checkbox" name="table[]" value="6"> <label for="six">Table de 6</label>
+                <input type="checkbox" name="table[]" value="7"> <label for="sept">Table de 7</label>
+                <input type="checkbox" name="table[]" value="8"> <label for=" huit">Table de 8</label>
+                <input type="checkbox" name="table[]" value="9"> <label for="neuf">Table de 9</label>
+                <input type="checkbox" name="table[]" value="10"> <label for="dix">Table de 10</label></p>
+            
+                <input type="submit" value="Afficher" name="afficher">
+        </form>
+
+<!-- Fin HTML formulaire checkbox -->
+
+<!-- Début PHP formulaire checkbox  -->
 
     <?php
-
-    $num = $_POST['check']; 
-    foreach($_POST['table'] as $valeur){
-    for ($num=1; $num <= 10; $num++) {
-    if ($num == 10){
-    
-    echo $valeur. ' x ' .$num. ' = ' .$num * $valeur.'<br></p>';
-    } else {
-    echo $valeur. ' x ' .$num. ' = ' .$num * $valeur.'<br>';
-    }
-    
-    }
+    $num = isset($_POST['check']) ? $_POST['check'] : NULL;
+    foreach($_POST['table'] as $valeur)
+    {
+        for ($num=1; $num <= 10; $num++) 
+        {
+            if ($num == 10)
+            {
+                echo $valeur. ' x ' .$num. ' = ' .$num * $valeur.'<br></p>';
+            } 
+            else 
+            {
+                echo $valeur. ' x ' .$num. ' = ' .$num * $valeur.'<br>';
+            }
+        
+        }
     } 
-    
     ?>
+    
+<!-- Fin PHP formulaire checkbox -->
 
-
+<!-- Début HTML liste déroulante  -->
 
 <form method="post">
     <select name="choix">
 
             <option value="1">Table du 1</option>
-        
             <option value="2">Table du 2</option>
-        
             <option value="3">Table du 3</option>
-        
             <option value="4">Table du 4</option>
-
             <option value="5">Table du 5</option>
-        
             <option value="6">Table du 6</option>
-        
             <option value="7">Table du 7</option>
-        
             <option value="8">Table du 8</option>
-
             <option value="9">Table du 9</option>
-        
             <option value="10">Table du 10</option>
         
         </select></p>
+
         <input type="submit" method="post" value="Valider" />
 </form>
+
+<!-- Fin HTML liste déroulante  -->
+
+<!-- Début PHP liste déroulante  -->
+
 <?php
-
-
 for ($j = 1; $j <= 10; $j++)
     {
         echo $_POST["choix"].' x '.$j.' = '. $_POST["choix"]*$j.'<br>';
     }
-
 ?>
+
+<!-- Fin PHP liste déroulante  -->
+
 </body>
 </html>
