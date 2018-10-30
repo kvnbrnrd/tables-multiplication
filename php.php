f959951840bd17a14ce281f3d3e7c2150c860246
kvnbrnrd <kevos.directos@gmail.com>
2018-10-30 11:39:21 +0100

    liste déroulante fonctionnelle

--- php.php
+++ php.php
@@ -1,8 +1,7 @@
-<PHP?> <link rel="stylesheet" type="text/css" href="index2.css"></head> <PHP>
-
+<head><link rel="stylesheet" type="text/css" href="index2.css"></head>
 <h1 class="multi">Multiplications</h1>
 
-<FORM>
+<form method="post">
     <h2>Affichage de plusieurs tables à la fois :</h2>
 
     <input type="hidden" name="categorie" id="categorie">
@@ -17,58 +16,41 @@
     <input type="checkbox"  value="9"> Table du 9
     <input type="checkbox"  value="10"> Table du 10</p>
     <button type="submit" formmethod="post">Valider</button></p>
-    </FORM>
+</form>
+
+<form method="post">
     <select name="choix">
 
-            <option value="choix1">Table du 1</option>
+            <option value="1">Table du 1</option>
         
-            <option value="choix2">Table du 2</option>
+            <option value="2">Table du 2</option>
         
-            <option value="choix3">Table du 3</option>
+            <option value="3">Table du 3</option>
         
-            <option value="choix4">Table du 4</option>
+            <option value="4">Table du 4</option>
 
-            <option value="choix5">Table du 5</option>
+            <option value="5">Table du 5</option>
         
-            <option value="choix6">Table du 6</option>
+            <option value="6">Table du 6</option>
         
-            <option value="choix7">Table du 7</option>
+            <option value="7">Table du 7</option>
         
-            <option value="choix8">Table du 8</option>
+            <option value="8">Table du 8</option>
 
-            <option value="choix9">Table du 9</option>
+            <option value="9">Table du 9</option>
         
-            <option value="choix10">Table du 10</option>
-
+            <option value="10">Table du 10</option>
         
         </select></p>
-
+        <input type="submit" method="post" value="Valider" />
+</form>
 <?php
 
 
-// for ($j = 1; $j <= 10; $j++)
-//     {
-//         echo '3'.' x '.$j.' = '. 3*$j.'<br>';
-//     }
-
-// $arr =
-
-for ($i = 1; $i <= 10; $i++)
-
-{
-    if($_POST['choix1'] == $i) {
-        echo '<p class="show">Table de multiplication de '.$i.'</p>'; 
-    }
-    else {
-        echo '<p class="hidden">Table de multiplication de '.$i.'</p>';
-    }
-
-   
-    for ($j = 1; $j <= 10; $j++)
+for ($j = 1; $j <= 10; $j++)
     {
-        echo $i.' x '.$j.' = '.$i*$j.'<br>';
+        echo $_POST["choix"].' x '.$j.' = '. $_POST["choix"]*$j.'<br>';
     }
-}
 
 ?>
 
